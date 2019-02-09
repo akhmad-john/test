951bf1addda646a664fec31ed800b6fdf96f5f1f
Akhmadjon Shamsuddinov <akhmad_shams@mail.ru>
2019-02-09 18:47:49 +0500

    first com

---
+++ index.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PN Group</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Khula:400,700" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet">
+<script
+  src="https://code.jquery.com/jquery-3.3.0.min.js"
+  integrity="sha256-RTQy8VOmNlT6b2PIRur37p6JEBZUE7o8wPgMvu18MC4="
+  crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+
+<link rel="stylesheet" type="text/css" href="stylesheet.css">
+
+
+
+</head>
+
+<body>
+<div class="container">
+<div class="header">
+  <a href="" class="logo"><img src="image/PNg.png"></a>
+  <div class="header-right">
+    <button type="button" class="btn  btn-sm"
+        data-toggle="toggle" data-target="#demo">
+    <i class="fa fa-share-alt"></i>
+</button>
+
+	<div id="demo" class="">
+	<a href="https://www.behance.net/pngroup"><button class="btn btn-sm" type="button">
+        <i class="fa fa-behance"></i></a>
+    </button>
+    <a href="https://dribbble.com/pngroup"><button class="btn btn-sm" type="button">
+        <i class="fa fa-dribbble"></i></a>
+    </button>
+    <a href="https://www.instagram.com/pn_group_co/"><button class="btn btn-sm" type="button">
+        <i class="fa fa-instagram"></i></a>
+    </button>
+    <a href="https://t.me/PN_Group"><button class="btn btn-sm" type="button">
+        <i class="fa fa-telegram"></i></a>
+    </button>
+     <a href="https://www.pinterest.com/pn_group/"><button class="btn btn-sm" type="button">
+        <i class="fa fa-pinterest"></i></a>
+    </button>
+	</div>
+
+  </div>
+</div>
+</div>
+<div class="container">
+  <form action="/action_page.php" class="form-group">
+  <div class="row label1">
+  	<div class="col-75 info">
+        <label for="contact_lab" class="lab">&nbspСвязаться с нами</label>
+      </div>
+  </div>
+    <div class="row">
+      <div class="col-75">
+        <input type="text" id="fname" name="firstname" placeholder="Ваше Ф.И.О">
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-75">
+        <input type="text" id="lname" name="lastname" placeholder="Ваш электронный адрес">
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-75">
+        <textarea id="subject" name="subject" placeholder="Ваш вопрос" style="height:100px;"></textarea>
+      </div>
+    </div>
+    <div class="row">
+      <input type="submit" value="Отправить">
+    </div>
+  </form>
+</div>
+
+
+<!-- FOOTER START -->
+<div class="footer">
+  <div class="container">
+  <div class="nav">
+<!--   <div class="nav-header">
+    <div class="nav-title">
+      JoGeek
+    </div>
+  </div> -->
+  <div class="nav-btn">
+    <label for="nav-check">
+      <span></span>
+      <span></span>
+      <span></span>
+    </label>
+  </div>
+  <input type="checkbox" id="nav-check">
+  <div class="nav-links">
+		<a class="link_to_file" href="//github.io/jo_geek" target="_blank">О НАС</a>
+	 <a class="link_to_file" href="http://stackoverflow.com/users/4084003/" target="_blank">УСЛУГИ</a>
+	 <a class="link_to_file" href="https://in.linkedin.com/in/jonesvinothjoseph" target="_blank">ПОРТФОЛИО</a>
+	 <a class="link_to_file" href="https://codepen.io/jo_Geek/" target="_blank">ВАКАНСИИ</a>
+	 <a class="link_to_file" href="https://jsfiddle.net/user/jo_Geek/" target="_blank">КОНТАКТЫ</a>
+  </div>
+  <div class="navbar navbar-default">
+  	<ul id="lang_select" class="navbar navbar-right">
+  		<li><number>+998998262408</number></li>
+  		<li class="underl"><a href="../pngroup" class="active">RU</a></li>
+  		<li><a href="uz.php">UZ</a></li>
+  	</ul>
+  </div>
+</div>
+</div>
+</div>
+ <script type="text/javascript" src="script.js"></script>
+
+</body>
+</html>
